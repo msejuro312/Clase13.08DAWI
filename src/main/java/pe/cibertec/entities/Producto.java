a60db1a44625dc851a7e368b7964cc797a32d952
msejuro312 <msejuro312@protonmail.com>
2026-08-13 19:30:16 -0500

    clase 13.08 sec config

---
+++ src/main/java/pe/cibertec/entities/Producto.java
@@ -0,0 +1,17 @@
+package pe.cibertec.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+    @Version
+    private Integer version; // Control de concurrencia optimista
+}
