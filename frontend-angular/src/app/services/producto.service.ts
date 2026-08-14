d83510cfdb3d762f46a06a3cccccead2e2b162e3
msejuro312 <msejuro312@protonmail.com>
2026-08-13 20:17:39 -0500

    angular2

---
+++ frontend-angular/src/app/services/producto.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductoService {
+  private apiUrl: string = 'http://localhost:8081/api/productos';
+
+  constructor(private http: HttpClient) {}
+
+  //el observable hace que se actualice solo
+  obtenerProducto(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+}
