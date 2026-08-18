146cf997a95a498b7a5ef2d822bacdf8fa7d0a30
msejuro312 <msejuro312@protonmail.com>
2026-08-18 13:14:16 -0500

    detalles

--- src/main/java/pe/cibertec/config/SecurityConfig.java
+++ src/main/java/pe/cibertec/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:4200"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET,POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
